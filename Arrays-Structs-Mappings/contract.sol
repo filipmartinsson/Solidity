fb21661e2614d9602d96d130a1da50cae0bb8e9c
Filip Martinsson <martinsson.filip@gmail.com>
2018-03-07 16:29:42 +0100

    prog

--- Arrays-Structs-Mappings/contract.sol
+++ Arrays-Structs-Mappings/contract.sol
@@ -10,13 +10,16 @@ contract DogContract{
     Dog[] dogs;
     mapping(address => uint) ownerToDog;
     
-    function addDog(string _name, uint _age){
+    event addedDog(address owner, string name, uint dogId);
+
+    function addDog(string _name, uint _age) internal {
         address owner = msg.sender;
         uint id = dogs.push(Dog(_name, _age));
         ownerToDog[owner] = id;
+        addedDog(owner, _name, id);
     }
     
-    function getDog() returns (string){
+    function getDog() returns (string) {
         address owner = msg.sender;
         uint id = ownerToDog[owner];
         return dogs[id-1].name;
