412cf96ee1640f7f899a595c80ab804c994bcf30
Filip Martinsson <martinsson.filip@gmail.com>
2018-05-23 10:58:06 +0200

    assignemnts

---
+++ Solidity Assignment solutions/Assignment2.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.0;
+
+contract Group{
+    Person[] ages;
+
+    struct Person{
+        string name;
+        uint age;
+    }
+
+    function addPerson(string _name, uint _age) {
+        ages.push(Person(_name, _age));
+    }
+
+    function getAvarageAge() returns (uint){
+        uint total;
+        for (uint i=0; i < ages.length; i++) {
+            total += ages[i].age;
+        }
+        return total/ages.length;
+    }
+
+}
